d5a8db2034c199f62e2015edc1e92ea3845a2b6a
Zeldariam <37988307+Zeldariam@users.noreply.github.com>
2022-11-12 16:06:07 +0100

    C Enseignant, EspritHashSet, EspritTreeSet || I GestionEnseignant

---
+++ src/List/Enseignant.java
@@ -0,0 +1,53 @@
+package List;
+
+import java.util.Objects;
+
+public class Enseignant {
+//ATTRIBUTES
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public int getId() {
+        return id;
+    }
+//GETTERS & SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+//CONSTRUCTORS
+    public Enseignant() {
+    }
+    public Enseignant(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enseignant that = (Enseignant) o;
+        return id == that.id;
+    }
+
+
+}
